8ba5b62be5a8ae6648b1f057664c7f92cc866ad9
DevLung <123473305+DevLung@users.noreply.github.com>
2024-12-10 22:19:19 +0100

    add error handling to transcode utility filename input

--- src/RLE-transcode.py
+++ src/RLE-transcode.py
@@ -1,5 +1,6 @@
-from sys import argv
+from sys import argv, stderr, exit
 import struct
+from os import path
 
 
 HEADER_LENGTH: int = 2
@@ -7,7 +8,24 @@ BLACK_PIXEL: str = "□"
 WHITE_PIXEL: str = "■"
 
 
-with open(argv[1], "rb") as file:
+if len(argv) > 1:
+    file_path: str = argv[1]
+    if not path.exists(file_path):
+        print("please supply a valid file path", file=stderr)
+        exit(1)
+else:
+    print("please enter a file path")
+    while True:
+        try:
+            file_path: str = input(" > ")
+            if path.exists(file_path):
+                break
+            print("please supply a valid file path", file=stderr)
+        except KeyboardInterrupt:
+            exit()
+
+
+with open(file_path, "rb") as file:
     data: bytes = file.read()
 
 width: int = struct.unpack(">H", data[:HEADER_LENGTH])[0]
